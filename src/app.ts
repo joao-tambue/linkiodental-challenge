e8b13c67fbf507b067f9676c2a752ac3b228ff3a
João Tambue <tjoao7279@gmail.com>
2025-12-27 02:21:56 +0100

    feat: configuring CORS for front access

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import authRoutes from './routes/auth.routes';
 import orderRoutes from './routes/order.routes';
 import dotenv from 'dotenv';
@@ -7,7 +8,15 @@ import './config/db';
 dotenv.config();
 
 const app = express();
+
 app.use(express.json());
+
+app.use(cors({
+	origin: 'http://localhost:5000',
+	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+	allowedHeaders: ['Content-Type', 'Authorization'],
+	credentials: true,
+}));
 
 app.use('/auth', authRoutes);
 app.use('/orders', orderRoutes);
